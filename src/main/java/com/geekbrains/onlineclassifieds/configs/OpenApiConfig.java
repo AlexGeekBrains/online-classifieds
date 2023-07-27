2debed813aa20fe76d50841fca6e7efb1d1356c9
unknown <89163278541@mail.ru>
2023-07-27 15:22:12 +0300

    add swagger and CustomPage

---
+++ src/main/java/com/geekbrains/onlineclassifieds/configs/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package com.geekbrains.onlineclassifieds.configs;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "online-classifieds",
+                version = "1.0.0",
+                contact = @Contact(
+                        name = "Sveshnikov Alexandr",
+                        email = "89163278541@mail.ru"
+                )
+        )
+)
+@SecurityScheme(
+        name = "JWT",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class OpenApiConfig {
+}
