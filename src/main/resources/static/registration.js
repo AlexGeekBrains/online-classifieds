21116ace113e1da314152d6b87b10d00b5862ff7
Andrey Shupeyko <andryws@mail.ru>
2023-08-03 09:44:21 +0300

    deleted response token

--- src/main/resources/static/registration.js
+++ src/main/resources/static/registration.js
@@ -1,8 +1,6 @@
 angular.module('app', []).controller('regController', function ($scope, $http) {
 
     $scope.addUser = function () {
-        $http.post('http://localhost:8080/online-classifieds/api/v1/authentication/registration', $scope.newUser).then(function (response) {
-            $scope.userToken = response.data;
-        });
+        $http.post('http://localhost:8080/online-classifieds/api/v1/authentication/registration', $scope.newUser);
     }
 });
