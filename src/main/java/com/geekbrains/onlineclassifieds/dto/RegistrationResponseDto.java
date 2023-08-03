e30740702bd00d3d67992e24511f442ab2d54f6c
Andrey Shupeyko <andryws@mail.ru>
2023-08-03 13:20:43 +0300

    fix

---
+++ src/main/java/com/geekbrains/onlineclassifieds/dto/RegistrationResponseDto.java
@@ -0,0 +1,12 @@
+package com.geekbrains.onlineclassifieds.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegistrationResponseDto {
+    private String message;
+}
--- src/main/resources/static/registration.js
+++ src/main/resources/static/registration.js
@@ -1,6 +1,15 @@
 angular.module('app', []).controller('regController', function ($scope, $http) {
 
     $scope.addUser = function () {
-        $http.post('http://localhost:8080/online-classifieds/api/v1/authentication/registration', $scope.newUser);
+        $http.post('http://localhost:8080/online-classifieds/api/v1/authentication/registration', $scope.newUser) .then(function(response) {
+         alert(response.data.message);
+            }) .catch(function(error) {
+                if (error.data && error.data.messages) {
+                alert('An error occurred: ' + error.data.messages.join(', '));
+                    } else {
+                    alert('An error occurred: ' + error.statusText);
+                    }
+         });
     }
-});
+ });
+
