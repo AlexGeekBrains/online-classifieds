5d7590297b6ef34e17c0b9521983b06941399c7b
Andrey Shupeyko <andryws@mail.ru>
2023-08-10 09:49:16 +0300

    fix

--- src/main/java/com/geekbrains/onlineclassifieds/services/CategoryServiceImpl.java
+++ src/main/java/com/geekbrains/onlineclassifieds/services/CategoryServiceImpl.java
@@ -1,17 +1,19 @@
 package com.geekbrains.onlineclassifieds.services;
 
+import com.geekbrains.onlineclassifieds.converters.CategoryConverter;
 import com.geekbrains.onlineclassifieds.entities.Category;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import com.geekbrains.onlineclassifieds.repositories.CategoryRepository;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class CategoryServiceImpl implements CategoryService {
     private final CategoryRepository categoryRepository;
-
+    private final CategoryConverter categoryConverter;
     @Override
     public Optional<Category> getCategoryById(Long id){
         return categoryRepository.findById(id);
@@ -23,6 +25,6 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public List findAllCategory() {
-        return categoryRepository.findAll();
+    return categoryRepository.findAll().stream().map(categoryConverter::entityToDto).collect(Collectors.toList());
     }
 }
